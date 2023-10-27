5e25dbaa0840fcdc4cecad1f457997a011ceb2af
St. Nurmuhsina <114470842+naadflwrx@users.noreply.github.com>
2023-10-27 12:27:51 +0800

    Update boilerplate.php

--- boilerplate.php
+++ boilerplate.php
@@ -1,71 +1,38 @@
 <?php
 
-# membuat class Animal
-class Animal
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
 {
-    # property animals
-    public $animals;
+    public $animals = ["Kucing", "Ayam", "Ikan", "Burung"];
 
-    # method constructor - mengisi data awal
-    # parameter: data hewan (array)
-    public function __construct($data)
-    {
-        $this->animals = $data;
-    }
-
-    # method index - menampilkan data animals
-    public function index()
-    {
-        # gunakan foreach untuk menampilkan data animals (array)
-        foreach ($this->animals as $animal){
+    public function index(){
+        foreach ($this->animals as $animal) {
             echo $animal;
             echo "<br>";
         }
     }
 
-    # method store - menambahkan hewan baru
-    # parameter: hewan baru
-    public function store($data)
-    {
-        # gunakan method array_push untuk menambahkan data baru
-        array_push ($this->animals, $data);
+    public function store(Request $request){
+        array_push ($this->animals, $request->nama);
+        echo "Nama hewan: $request->nama";
+        echo "<br>";
+        echo "Menambahkan hewan baru";
     }
 
-    # method update - mengupdate hewan
-    # parameter: index dan hewan baru
-    public function update($index, $data)
-    {
-        $this->animals[$index] = $data;
+    public function update(Request $request, $id){
+        $this->animals[$index] = $request->nama;
+        echo "Nama hewan: $request->nama";
+        echo "<br>";
+        echo "Mengupdate data hewan id $id";
     }
 
-    # method delete - menghapus hewan
-    # parameter: index
-    public function destroy($index)
-    {
-        # gunakan method unset atau array_splice untuk menghapus data array
-        array_splice ($this->animals, $index, 1);
+    public function destroy($id){
+        array_splice ($this->animals, $id, 1);
+        echo "Menghapus data hewan id $id";
     }
+
 }
-
-# membuat object
-# kirimkan data hewan (array) ke constructor
-$animal = new Animal(["Ayam", "Ikan"]);
-
-echo "Index - Menampilkan seluruh hewan <br>";
-$animal->index();
-echo "<br>";
-
-echo "Store - Menambahkan hewan baru <br>";
-$animal->store("Burung");
-$animal->index();
-echo "<br>";
-
-echo "Update - Mengupdate hewan <br>";
-$animal->update(0, 'Kucing Anggora');
-$animal->index();
-echo "<br>";
-
-echo "Destroy - Menghapus hewan <br>";
-$animal->destroy(1);
-$animal->index();
-echo "<br>";
