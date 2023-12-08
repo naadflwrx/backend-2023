881db1df0b0b78fb7f4da53effb87335d83014da
naadflwrx <itssinaa1904@gmail.com>
2023-12-08 11:03:00 +0700

    pertemuan9

---
+++ data/fruit.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["jeruk","apel","durian"];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
