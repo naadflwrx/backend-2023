e21fa45e8df66702edf6f8c6677067bd1c4acb2e
naadflwrx <itssinaa1904@gmail.com>
2024-01-19 18:26:49 +0700

    pertemuan12

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const StudentController = require('../controllers/StudentController.js');
+const express = require('express');
+
+const router = express.Router();
+
+// mendefinisikan route
+router.get('/', (req, res) => {
+    res.send('Hai Express!');
+});
+
+// Routing student
+router.get('/students', StudentController.index);
+router.post('/students', StudentController.store);
+router.put("/students/:id", StudentController.update);
+router.delete("/students/:id", StudentController.destroy);
+
+// export router
+module.exports = router;
